66f3110913224760d928820ea28a89053643cc5d
Mark K. <s3992118@student.rmit.edu.au>
2025-02-26 23:34:49 +1100

    Add three.js components and loader; refactor Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,25 @@
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import {Canvas} from "@react-three/fiber";
+
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
 
 
 const Hero = () => {
+    const isSmall = useMediaQuery({maxWidth: 440 } );
+    const isMobile = useMediaQuery({maxWidth: 768 } );
+    const isTablet = useMediaQuery({maxwidth: 768, maxWidth: 1024 } );
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -9,8 +28,26 @@ const Hero = () => {
             </div>
 
             <div className="w-full h-full absolute inset-0">
-                <Canvas>
+                {/*<Leva />*/}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                    <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+                    <HackerRoom
+                        position={sizes.deskPosition}
+                        scale={sizes.deskScale}
+                        rotation={[0, -Math.PI, 0]}
+                    />
 
+                        <group>
+                                <Target position={sizes.targetPosition}/>
+                            <ReactLogo position={sizes.reactLogoPosition}/>
+                            <Cube position={sizes.cubePosition}/>
+                            <Rings position={sizes.ringPosition}/>
+                        </group>
+
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
                 </Canvas>
             </div>
 
