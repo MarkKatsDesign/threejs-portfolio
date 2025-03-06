6461c24abc56c966b6a66152577e2a43128568fa
Mark K. <s3992118@student.rmit.edu.au>
2025-03-06 23:43:40 +1100

    Enhance About section; update text for clarity and add DemoComputer component with improved lighting and controls

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,14 +17,14 @@ const About = () => {
     }
 
     return (
-        <section className="c-space my-20">
+        <section className="c-space my-20" id="about">
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="xl:col-span-1 xl:row-span-3">
                     <div className="grid-container">
                         <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
                         <div>
                             <p className="grid-headtext">Hi, I'm Mark</p>
-                            <p className="grid-subtext">With 3 years design and 7 months of full-stack development, I have honed my skills in frontend and backend development with a focus in animated websites.</p>
+                            <p className="grid-subtext">With 3 years of design and 7 months of full-stack development experience, I have honed my skills in frontend and backend development with a focus in animated websites.</p>
                         </div>
                     </div>
                 </div>
